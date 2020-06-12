cb0c1a30a310380c9da07f4cfbcb04e5928d9957
Pavel Zhdanovich <zhdan21@gmail.com>
2020-06-12 12:36:31 +0300

    initial commit

---
+++ webpack.config.js
@@ -0,0 +1,7 @@
+const path = require('path');
+const HtmlWebPackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+module.exports = {
+  entry: './js/app.js',
+};
